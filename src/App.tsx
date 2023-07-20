4079807955bc75a57068f171830d84eeaecd6600
sergio_manica <serginhomanica@hotmail.com>
2023-07-20 00:16:21 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import "./App.css";
+import { Form } from "./components/Form";
+import { Success } from "./components/Success";
+import backgroundImageDesktop from "./assets/bg-main-desktop.png";
+import backgroundImageMobile from "./assets/bg-main-mobile.png";
+import cardFrontImage from "./assets/bg-card-front.png";
+import cardBackImage from "./assets/bg-card-back.png";
+import cardLogo from "./assets/card-logo.svg";
+
+const initialInputState = {
+  value: "",
+  isTouched: false,
+};
+
+function App() {
+  const [cardholderName, setCardholderName] = useState(initialInputState);
+  const [cardNumber, setCardNumber] = useState(initialInputState);
+  const [expirationMonth, setExpirationMonth] = useState(initialInputState);
+  const [expirationYear, setExpirationYear] = useState(initialInputState);
+  const [cvc, setCvc] = useState(initialInputState);
+
+  const [isFormComplete, setIsFormComplete] = useState(false);
+
+  function handleCompleteForm() {
+    setIsFormComplete(true);
+  }
+
+  return (
+    <div className="page-container">
+      <div className="image-container">
+        <img className="mobile-background" src={backgroundImageMobile} alt="" />
+        <img className="desktop-background" src={backgroundImageDesktop} alt="" />
+        <div className="card-front">
+          <img src={cardFrontImage} alt="" />
+          <div className="card-info">
+            <img src={cardLogo} alt="" />
+            <span className="card-number">{cardNumber.value || '0000 0000 0000 0000'}</span>
+            <span>{cardholderName.value || 'John Doe'}</span>
+            <span>{expirationMonth.value || '00'}/{expirationYear.value || '00'}</span>
+          </div>
+        </div>
+        <div className="card-back">
+          <img src={cardBackImage} alt="" />
+          <div className="card-info">
+            <span className="card-cvc">{cvc.value || '000'}</span>
+          </div>
+        </div>
+      </div>
+      <main className="main-content">
+        {isFormComplete ? (
+          <Success />
+        ) : (
+          <Form
+            cardholderName={cardholderName}
+            setCardholderName={setCardholderName}
+            cardNumber={cardNumber}
+            setCardNumber={setCardNumber}
+            expirationMonth={expirationMonth}
+            setExpirationMonth={setExpirationMonth}
+            expirationYear={expirationYear}
+            setExpirationYear={setExpirationYear}
+            cvc={cvc}
+            setCvc={setCvc}
+            onCompleteForm={handleCompleteForm}
+          />
+        )}
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Success.tsx
@@ -0,0 +1,15 @@
+import successSvg from "../assets/icon-complete.svg";
+
+export function Success() {
+  return (
+    <div className="success-container">
+      <img src={successSvg} alt="" />
+      <div className="success-content">
+        <h1 className="success-title">Thank You!</h1>
+        <p className="success-description">We've added your card details</p>
+      </div>
+
+      <button className="button">Continue</button>
+    </div>
+  );
+}
